ef5b5fd106a6bdba655e5df1c270cd1fedb38cf6
GmHadou <106123785+GMHadou@users.noreply.github.com>
2024-05-09 14:20:30 -0300

    Update Shrinkage.py

--- Shrinkage.py
+++ Shrinkage.py
@@ -14,7 +14,7 @@ Layer_Height=0.2
 Wall_thickness=0.8
 #The density is given in percentage,the higher the density,the higher the shrinkage
 Infill_density=0.1
-#THe actual heat dispersion is influenced by the number of walls,gaps between the infill,density of the infill and type of infill
+#The actual heat dispersion is influenced by the number of walls,gaps between the infill,density of the infill and type of infill
 #Volume plays a massive role in shrinkage.THe internal structure of the mesh is defined by many variables,some of which are considered to make this calculus work
 surface=mesh.extract_surface()
 
@@ -24,6 +24,8 @@ Outer_volume=Surface_Area*Wall_thickness
 
 total_volume = mesh.volume
 
+#Even though CTE and Fluid_Fase are not used,they are present to correspond to possible calculus that are closer to a
+theoretical value of shrinkage
 CTE = 68e-6  # Coefficient of Thermal Expansion for PLA in /°C
 initial_temp = 210  # Initial temperature in °C
 Fluid_Fase = 135  # Temperature of the start of the Phase that Plastic is molded
@@ -38,6 +40,7 @@ Shrinkage_percentage=Shrinkage_volume_surface2/Outer_volume
 print(Shrinkage_volume_surface2/Outer_volume,Area_shrinkage)
 
 
+#Volume Safety calculation for toppling analysis
 def volume_safety(Shrinkage_volume_surface2, Outer_volume, heat_warped):
     if Shrinkage_volume_surface2/Outer_volume < 0.05 or heat_warped < 40:
         unstable = 1
